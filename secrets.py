d00218670c8739f58f60b605f3a00ed255764354
Molly Wehrenberg <mwehrenberg2@gmail.com>
2024-10-15 09:59:11 -0700

    Delete secrets.py

--- secrets.py
+++
@@ -1,8 +0,0 @@
-presigned_url = "https://your-bucket-name.s3.amazonaws.com/your-object-key?AWSAccessKeyId=AKIAIOSFODNN7EXAMPLE&Expires=1609459200&Signature=EXAMPLE"
-
-# This is a configuration file for connecting to Azure Database for MySQL
-
-# Database connection string
-DB_CONNECTION_STRING="hostname=mwehr-databasetest.mysql.database.azure.com port=3306 username=mwehrenberg password=Thisdoesntmatter123! ssl-mode=require"
-
-# Note: Ensure that this file is not exposed publicly and is used only for testing secret scanning.
